f3ce0b0a9b5dbae973d042ea70e8017c9d28c2df
Paul Logston <code@logston.me>
2014-01-02 23:54:36 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='py3s3',
+      version='0.1.0',
+      description='A bare bones package for upload and downloading '
+                  'to AWS S3 from within Python 3.',
+      url='http://github.com/logston/py3s3',
+      author='Paul Logston',
+      author_email='code@logston.me',
+      license='GPLv3',
+      packages=['py3s3'],
+      zip_safe=False)
